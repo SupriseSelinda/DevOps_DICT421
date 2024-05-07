b7956fcbdf5075a3e353b855255ecd05124b89c0
SupriseSelinda <selindasurprise22@gmail.com>
2024-05-07 14:46:56 +0200

    commit

---
+++ src/main/java/com/mycompany/selindasuprise220792844/SelindaSuprise220792844.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.selindasuprise220792844;
+
+/**
+ *
+ * @author Suprise
+ */
+public class SelindaSuprise220792844 {
+
+    public static void main(String[] args) {
+         
+        int firstNumber = 10;
+        int secondNumber = 20;
+        int sum = firstNumber + secondNumber;
+        System.out.println("The sum of " + firstNumber + " and " + secondNumber + " is: " + sum);
+    }
+}
